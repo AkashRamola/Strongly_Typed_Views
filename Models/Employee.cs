68b4dc5dd347b550ea7a12758fcf75834aad5a60
Akash Chand Ramola <akashc@unifycloud.com>
2026-09-23 15:20:16 +0530

    Add model class and dbcontext

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strongly_Typed_Views.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public double Salary { get; set; }
+}
